b1004d49c0c92ab07a27b19deeb297ed03387f14
Daniel Kanzel <daniil.ershov@inplat.ru>
2018-09-26 17:51:38 +0300

    почти заработало

--- example.py
+++ example.py
@@ -1,21 +1,25 @@
 from xml.etree import ElementTree
+import csv
 
 tree = ElementTree.parse('input3.xml')
 root = tree.getroot()
 
-for child_of_root in root:
-    docparams = child_of_root.attrib
-    for subchild in child_of_root.iter('СведНП'):
-        name = subchild.attrib
-    for subsubchild in child_of_root.iter('СведССЧР'):
-        another = subsubchild.attrib
-        result = dict(docparams)
-        result.update(name)
-        result.update(another)
-        print(result)
+with open('output.csv', 'w') as f:
+    for child_of_root in root:
+        docparams = child_of_root.attrib
+        for subchild in child_of_root.iter('СведНП'):
+            name = subchild.attrib
+        for subsubchild in child_of_root.iter('СведССЧР'):
+            another = subsubchild.attrib
+            result = dict(docparams)
+            result.update(name)
+            result.update(another)
+            w.writeheader()
+            w = csv.DictWriter(f, result.keys())
+            w.writerow(result)
+            print (result)
 
-#for att in root:
- #   first = att.find('attval').text
- #   for subatt in att.find('children'):
- #       second = subatt.find('attval').text
- #       print('{},{}'.format(first, second))
+
+
+
+
